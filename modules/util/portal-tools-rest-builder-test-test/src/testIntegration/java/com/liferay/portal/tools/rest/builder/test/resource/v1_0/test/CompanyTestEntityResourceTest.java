bdbc81cb048b7a0b30d6a61faeffe993734c4b90
Mauricio Valdivia <mauricio.valdivia@liferay.com>
2025-04-13 22:22:56 +0200

    LPD-45648 add migrated test from poshi to CompanyTestEntityResourceTest. Update and post using batch.

--- modules/util/portal-tools-rest-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/rest/builder/test/resource/v1_0/test/CompanyTestEntityResourceTest.java
+++ modules/util/portal-tools-rest-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/rest/builder/test/resource/v1_0/test/CompanyTestEntityResourceTest.java
@@ -6,9 +6,27 @@
 package com.liferay.portal.tools.rest.builder.test.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.test.util.HTTPTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.tools.rest.builder.test.client.dto.v1_0.CompanyTestEntity;
+import com.liferay.portal.tools.rest.builder.test.client.resource.v1_0.CompanyTestEntityResource;
+import com.liferay.portal.kernel.model.User;
 
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import com.liferay.portal.util.PropsValues;
 
 /**
  * @author Alejandro Tardín
@@ -68,6 +86,22 @@ public class CompanyTestEntityResourceTest
 	}
 
 	@Override
+	@Test
+	public void testPatchCompanyTestEntity() throws Exception {
+		super.testPatchCompanyTestEntity();
+
+		_testPatchCompanyTestEntityBatch();
+	}
+
+	@Override
+	@Test
+	public void testPostCompanyTestEntity() throws Exception {
+		super.testPostCompanyTestEntity();
+
+		_testPostCompanyTestEntityBatch();
+	}
+
+	@Override
 	protected CompanyTestEntity testPostCompanyTestEntity_addCompanyTestEntity(
 			CompanyTestEntity companyTestEntity)
 		throws Exception {
@@ -112,4 +146,140 @@ public class CompanyTestEntityResourceTest
 			randomCompanyTestEntity());
 	}
 
+	@Override
+	protected CompanyTestEntity testPatchCompanyTestEntity_addCompanyTestEntity()
+		throws Exception {
+
+		return companyTestEntityResource.postCompanyTestEntity(
+			randomCompanyTestEntity());
+	}
+
+	private void _testPatchCompanyTestEntityBatch() throws Exception {
+		CompanyTestEntity postCompanyTestEntity = new CompanyTestEntity(){{externalReferenceCode = RandomTestUtil.randomString();}};
+
+		CompanyTestEntity notExistingPostCompanyTestEntity = new CompanyTestEntity();
+
+		postCompanyTestEntity.setDescription(
+			StringUtil.toLowerCase(RandomTestUtil.randomString()));
+
+		CompanyTestEntity companyTestEntity =
+			companyTestEntityResource.postCompanyTestEntity(
+				postCompanyTestEntity);
+
+		postCompanyTestEntity.setId(companyTestEntity.getId());
+
+		postCompanyTestEntity.setDescription(RandomTestUtil.randomString());
+
+		notExistingPostCompanyTestEntity.setDescription(
+			StringUtil.toLowerCase(RandomTestUtil.randomString()));
+		notExistingPostCompanyTestEntity.setId(companyTestEntity.getId() + 1);
+
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.batch.engine.internal.strategy." +
+					"OnErrorContinueBatchEngineImportStrategy",
+				LoggerTestUtil.ERROR)) {
+
+			_waitForFinish(
+				"COMPLETED", true,
+				JSONFactoryUtil.createJSONObject(
+					createCompanyTestEntityResourceWithParameters(
+						new String[] {
+							"updateStrategy", "UPDATE", "importStrategy",
+							"ON_ERROR_CONTINUE"
+						}
+					).putCompanyTestEntityBatchHttpResponse(
+						null, 
+						JSONUtil.putAll(
+							JSONFactoryUtil.createJSONObject(
+								String.valueOf(postCompanyTestEntity)),
+							JSONFactoryUtil.createJSONObject(
+								String.valueOf(notExistingPostCompanyTestEntity)))
+					).getContent()));
+		}
+
+		companyTestEntity = companyTestEntityResource.getCompanyTestEntity(
+			companyTestEntity.getId());
+
+		Assert.assertEquals(
+			postCompanyTestEntity.getId(), companyTestEntity.getId());
+		Assert.assertEquals(
+			postCompanyTestEntity.getDescription(),
+			companyTestEntity.getDescription());
+	}
+
+	private void _testPostCompanyTestEntityBatch() throws Exception {
+		CompanyTestEntity companyTestEntity = companyTestEntityResource.postCompanyTestEntity(
+			new CompanyTestEntity() {{
+				externalReferenceCode = RandomTestUtil.randomString();
+			}}
+		);
+
+		companyTestEntity.setId(companyTestEntity.getId() + 1);
+
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.batch.engine.internal.BatchEngineImportTaskExecutorImpl",
+				LoggerTestUtil.ERROR)) {
+
+			_waitForFinish(
+				"FAILED", true,
+				JSONFactoryUtil.createJSONObject(
+					companyTestEntityResource.postCompanyTestEntityBatchHttpResponse(
+						null,
+						JSONUtil.putAll(
+							JSONFactoryUtil.createJSONObject(String.valueOf(companyTestEntity)))
+					).getContent()));
+		}
+	}
+
+	private JSONObject _waitForFinish(
+			String expectedExecuteStatus, boolean importTask,
+			JSONObject jsonObject)
+		throws Exception {
+
+		String endpoint = StringBundler.concat(
+			"headless-batch-engine/v1.0/",
+			importTask ? "import-task" : "export-task",
+			"/by-external-reference-code/");
+
+		while (true) {
+			jsonObject = HTTPTestUtil.invokeToJSONObject(
+				null, endpoint + jsonObject.getString("externalReferenceCode"),
+				Http.Method.GET);
+
+			String executeStatus = jsonObject.getString("executeStatus");
+
+			if (StringUtil.equals(executeStatus, "COMPLETED") ||
+				StringUtil.equals(executeStatus, "FAILED")) {
+
+				Assert.assertEquals(expectedExecuteStatus, executeStatus);
+
+				return jsonObject;
+			}
+		}
+	}
+
+	private CompanyTestEntityResource createCompanyTestEntityResourceWithParameters(
+			String[] parameters)
+		throws Exception {
+
+		testCompany = CompanyLocalServiceUtil.getCompany(
+			testGroup.getCompanyId());
+
+		User testCompanyAdminUser = UserTestUtil.getAdminUser(
+			testCompany.getCompanyId());
+
+		CompanyTestEntityResource companyTestEntityResource = CompanyTestEntityResource.builder(
+		).authentication(
+			testCompanyAdminUser.getEmailAddress(),
+			PropsValues.DEFAULT_ADMIN_PASSWORD
+		).endpoint(
+			testCompany.getVirtualHostname(), 8080, "http"
+		).locale(
+			LocaleUtil.getDefault()
+		).parameters(
+			parameters
+		).build();
+
+		return companyTestEntityResource;
+	}
 }
