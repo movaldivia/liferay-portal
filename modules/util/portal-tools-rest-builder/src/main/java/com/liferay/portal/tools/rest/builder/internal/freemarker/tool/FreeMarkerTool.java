8eece1fa89a22dacbb57c45353a3d7492406ece9
Mauricio Valdivia <mauricio.valdivia@liferay.com>
2025-05-06 12:27:01 +0200

    LPD-54467 sf

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/FreeMarkerTool.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/FreeMarkerTool.java
@@ -652,8 +652,6 @@ public class FreeMarkerTool {
 		return null;
 	}
 
-
-
 	public Map<String, Schema> getMultipartBodySchemas(
 		JavaMethodSignature javaMethodSignature) {
 
@@ -923,6 +921,44 @@ public class FreeMarkerTool {
 			configYAML, true, openAPIYAML, schema, schemas);
 	}
 
+	public boolean hasCorrespondingMethodWithMediaType(
+		List<JavaMethodSignature> javaMethodSignatures,
+		JavaMethodSignature sourceJavaMethodSignature,
+		String targetMethodPrefix, String sourceMethodPrefix,
+		String... mediaTypes) {
+
+		if ((sourceJavaMethodSignature == null) ||
+			!sourceJavaMethodSignature.getMethodName(
+			).startsWith(
+				sourceMethodPrefix
+			)) {
+
+			return false;
+		}
+
+		String methodSuffix = sourceJavaMethodSignature.getMethodName(
+		).substring(
+			sourceMethodPrefix.length()
+		);
+
+		String targetMethodName = targetMethodPrefix + methodSuffix;
+
+		JavaMethodSignature targetJavaMethodSignature = getJavaMethodSignature(
+			javaMethodSignatures, targetMethodName);
+
+		if (targetJavaMethodSignature == null) {
+			return false;
+		}
+
+		for (String mediaType : mediaTypes) {
+			if (hasRequestBodyMediaType(targetJavaMethodSignature, mediaType)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	public boolean hasHTTPMethod(
 		JavaMethodSignature javaMethodSignature, String... httpMethods) {
 
@@ -1048,37 +1084,6 @@ public class FreeMarkerTool {
 		}
 
 		return true;
-	}
-
-	public boolean hasCorrespondingMethodWithMediaType(
-		List<JavaMethodSignature> javaMethodSignatures,
-		JavaMethodSignature sourceJavaMethodSignature,
-		String targetMethodPrefix,
-		String sourceMethodPrefix,
-		String... mediaTypes) {
-
-		if (sourceJavaMethodSignature == null ||
-			!sourceJavaMethodSignature.getMethodName().startsWith(sourceMethodPrefix)) {
-			return false;
-		}
-
-		String methodSuffix = sourceJavaMethodSignature.getMethodName().substring(sourceMethodPrefix.length());
-		String targetMethodName = targetMethodPrefix + methodSuffix;
-
-		JavaMethodSignature targetJavaMethodSignature =
-			getJavaMethodSignature(javaMethodSignatures, targetMethodName);
-
-		if (targetJavaMethodSignature == null) {
-			return false;
-		}
-
-		for (String mediaType : mediaTypes) {
-			if (hasRequestBodyMediaType(targetJavaMethodSignature, mediaType)) {
-				return true;
-			}
-		}
-
-		return false;
 	}
 
 	public boolean isCollection(
